f040d38d928610d981102f176365841b84c470d5
league <league@wts9.attlocal.net>
2019-04-05 18:26:51 -0700

    Tutor Typing(Continue), Chuckle Clicker

---
+++ src/fortune_teller/Typing_Tutor.java
@@ -0,0 +1,53 @@
+package fortune_teller;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Typing_Tutor implements KeyListener {
+static JFrame JF = new JFrame("Type");
+static JPanel JP = new JPanel();
+static JLabel JL = new JLabel();
+char currentLetter;
+public static void main(String[] args) {
+	Typing_Tutor tt = new Typing_Tutor();
+	tt.createButtons();
+}	
+
+void createButtons() {
+currentLetter = generateRandomLetter();
+JF.setVisible(true);
+JF.add(JP);
+JL.setFont(JL.getFont().deriveFont(28.0f));
+JL.setHorizontalAlignment(JL.CENTER);
+JF.addKeyListener(this);
+}
+
+char generateRandomLetter() {
+      Random r = new Random();
+      return (char) (r.nextInt(26) + 'a');	
+		
+}
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	currentLetter = generateRandomLetter();
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+}
--- src/jeopardy/Chuckle_Clicker.java
+++ src/jeopardy/Chuckle_Clicker.java
@@ -1,15 +1,46 @@
 package jeopardy;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class Chuckle_Clicker {
-public static void main(String[] args) {
-	
-}
+public class Chuckle_Clicker implements ActionListener {
+	 JButton JB = new JButton("Joke");
+		JButton Jb = new JButton("PunchLine");
+		JFrame JF = new JFrame();
+		JPanel JP = new JPanel();
+	public static void main(String[] args) {
+	Chuckle_Clicker c = new Chuckle_Clicker();
+	c.makeButtons();
+
+
+	}
  void makeButtons() {
-	JButton JB = new JButton();
-	JButton Jb = new JButton();
-	JFrame JF = new JFrame();
+	
+	JF.setTitle("Chuckle Clicker");
+	JF.setVisible(true);
+	JF.add(JP);
+	JP.add(Jb);
+	JP.add(JB);
+	JF.pack();
+JB.addActionListener(this); 
+ Jb.addActionListener(this);
+
  }
+ public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	 JButton pressed = (JButton) e.getSource();
+
+	 if(JB == pressed) {
+		 JOptionPane.showMessageDialog(null,"The joke is, we all have the same punchline");
+	 }
+	 else if(Jb == pressed) {
+	JOptionPane.showMessageDialog(null, "But in the end I wanna thank you. Because you make me that much stronger.");
+}
+ }
+
 }
