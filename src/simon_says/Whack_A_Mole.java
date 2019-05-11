cc8579671d396f8094ae0ba983810f2ea76593fd
league <league@wts9.attlocal.net>
2019-05-10 18:27:40 -0700

    WhackA Mole

--- src/simon_says/Whack_A_Mole.java
+++ src/simon_says/Whack_A_Mole.java
@@ -1,62 +1,86 @@
 package simon_says;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Whack_A_Mole {
+public class Whack_A_Mole implements ActionListener {
 	JFrame JF = new JFrame("Whack a Button");
 	JPanel JP = new JPanel();
 	JLabel JL = new JLabel();
-
+	static Date d = new Date();
 	void setup() {
 		JF.setVisible(true);
 		JF.add(JP);
 		JP.add(JL);
-
 		JL.setVisible(true);
+		JF.setPreferredSize(new Dimension(370, 300));
+		makeButtons();
 		JF.pack();
+
+	}
+
+	public static void main(String[] args) {
+		Whack_A_Mole mole = new Whack_A_Mole();
+		mole.setup();
+	}
+
+	void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	void makeButtons() {
-		JButton JB = new JButton();
-		JP.add(JB);
-		JButton JBu = new JButton();
-		JP.add(JBu);
-		JButton JBut = new JButton();
-		JF.add(JBut);
-		JButton JButt = new JButton();
-		JF.add(JButt);
-		JButton JButto = new JButton();
-		JP.add(JButto);
-		JButton JButton = new JButton();
-		JP.add(JButton);
-		JButton Jb = new JButton();
-		JP.add(Jb);
-		JButton Jbu = new JButton();
-		JP.add(Jbu);
-		JButton Jbut = new JButton();
-		JP.add(Jbut);
-		JButton Jbutt = new JButton();
-		JP.add(Jbutt);
-		JButton Jbutto = new JButton();
-		P.add(Jbutto);
-		JButton Jbutton = new JButton();
-		JP.add(Jbutton);
-		JButton JBU = new JButton();
-		JP.add(JBU);
-		JButton JBUT = new JButton();
-		JP.add(JBUT);
-		JButton JBUTT = new JButton();
-		JP.add(JBUTT);
-		JButton JBUTTO = new JButton();
-		JP.add(JBUTTO);
-		JButton JBUTTON = new JButton();
-		JP.add(JBUTTON);
-		JButton JBuT = new JButton();
-		JP.add(JBuT);
-
+		Random r = new Random();
+		int t = r.nextInt(24); 
+		for (int i = 0; i < 24; i++) {
+			JButton JB = new JButton();
+			JP.add(JB);
+			JB.addActionListener(this);
+			if(t == i) {
+				JB.setText("MOLE!");
+			
+			}
+		
+		}
+		
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton pressed = (JButton) e.getSource();
+		if(pressed.getText().equals("MOLE!")) {
+			JF.dispose();
+			JF= new JFrame();
+			JF.setVisible(true);
+			JP = new JPanel();
+			JF.add(JP);
+			JP.add(JL);
+			makeButtons();
+			JF.setPreferredSize(new Dimension(370, 300));
+			speak("You missed");
+			endGame(d, 10);
+		}
+			JF.pack();
+		}
+	
+	private void endGame(Date timeAtStart, int molesWhacked) {
+	     Date timeAtEnd = new Date();
+	     JOptionPane.showMessageDialog(null, "Your whack rate is "
+	          + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	          + " moles per second.");
+	}
 }
